db6149c90dcc5f13c16e8ff0a59dd9a189bd53bd
Faird1 <144528741+Faird1@users.noreply.github.com>
2025-12-07 22:32:18 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,6 +13,12 @@ include device/xiaomi/sm8450-common/BoardConfigCommon.mk
 # Inherit from the proprietary version
 include vendor/xiaomi/ingres/BoardConfigVendor.mk
 
+# Miui Camera for cupid
+include device/xiaomi/miuicamera-cupid/BoardConfig.mk
+
+# Vendor gamekey additions (if present)
+-include vendor/xiaomi/gamekey/BoardConfigVendor.mk
+
 DEVICE_PATH := device/xiaomi/ingres
 
 # Audio
