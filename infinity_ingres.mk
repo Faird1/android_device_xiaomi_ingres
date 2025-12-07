87e80aa751c580f6b5b4436cdd2fc88b8b1ea70d
Faird1 <144528741+Faird1@users.noreply.github.com>
2025-12-07 22:35:01 +0100

    Update and rename lineage_ingres.mk to infinity_ingres.mk

--- infinity_ingres.mk
+++ infinity_ingres.mk
@@ -8,12 +8,19 @@
 $(call inherit-product, device/xiaomi/ingres/device.mk)
 
 # Inherit from common lineage configuration
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_ingres
+# InfinityX
+TARGET_DISABLE_EPPE := true
+INFINITY_BUILD_TYPE := UNOFFICIAL
+INFINITY_MAINTAINER := "faridd"
+TARGET_HAS_UDFPS := true
+WITH_GAPPS := true
+
+PRODUCT_NAME := infinity_ingres
 PRODUCT_DEVICE := ingres
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_BRAND := Redmi
+PRODUCT_BRAND := POCO
 PRODUCT_MODEL := 21121210G
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
